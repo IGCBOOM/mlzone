965fd6324888969c02f22a82af605fbb461e739d
Carter <caya1.carter1@gmail.com>
2025-03-17 21:40:31 -0500

    tree plot.

--- ch3init.py
+++ ch3init.py
@@ -1,8 +1,10 @@
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import Perceptron
 from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
 from sklearn.metrics import accuracy_score
 
 from matplotlib.colors import ListedColormap
@@ -25,20 +27,25 @@ xTrainStd = sc.transform(xTrain)
 xTestStd = sc.transform(xTest)
 
 #ppn = Perceptron(eta0=0.1, random_state=1)
-lr = LogisticRegression(C=10.0, random_state=21, solver='lbfgs', multi_class='multinomial')
-lr.fit(xTrainStd, yTrain)
+#lr = LogisticRegression(C=10.0, random_state=21, solver='lbfgs', multi_class='multinomial')
+#svm = SVC(kernel="rbf", C=1.0, gamma=100, random_state=21)
+dtree = DecisionTreeClassifier(criterion="gini", max_depth=6, random_state=21)
+dtree.fit(xTrainStd, yTrain)
 
-yPred = lr.predict(xTestStd)
+yPred = dtree.predict(xTestStd)
 print("Missclassified examples: %d" % (yTest != yPred).sum())
 print("Accuracy: %.3f" % accuracy_score(yTest, yPred))
 
 xCombinedStd = np.vstack((xTrainStd, xTestStd))
 yCombined = np.hstack((yTrain, yTest))
 
-plotDecisionRegions(X=xCombinedStd, y=yCombined, classifier=lr, testIdx=range(105, 150))
+plotDecisionRegions(X=xCombinedStd, y=yCombined, classifier=dtree, testIdx=range(105, 150))
 plt.xlabel("Petal Length (Standardized)")
 plt.ylabel("Petal Width (Standardized)")
 plt.legend(loc="upper left")
 plt.tight_layout()
 plt.show()
 
+
+tree.plot_tree(dtree)
+plt.show()
