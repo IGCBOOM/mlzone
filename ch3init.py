3f94831c73d94154b7cd5de352368af0d91fc865
Carter <caya1.carter1@gmail.com>
2025-03-17 23:01:48 -0500

    KNN

--- ch3init.py
+++ ch3init.py
@@ -5,6 +5,8 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 
 from matplotlib.colors import ListedColormap
@@ -29,17 +31,19 @@ xTestStd = sc.transform(xTest)
 #ppn = Perceptron(eta0=0.1, random_state=1)
 #lr = LogisticRegression(C=10.0, random_state=21, solver='lbfgs', multi_class='multinomial')
 #svm = SVC(kernel="rbf", C=1.0, gamma=100, random_state=21)
-dtree = DecisionTreeClassifier(criterion="gini", max_depth=6, random_state=21)
-dtree.fit(xTrainStd, yTrain)
+#dtree = DecisionTreeClassifier(criterion="gini", max_depth=3, random_state=21)
+#forest = RandomForestClassifier(criterion="gini", n_estimators=50, random_state=1, n_jobs=4)
+knn = KNeighborsClassifier(n_neighbors=5, p=2, metric="minkowski")
+knn.fit(xTrainStd, yTrain)
 
-yPred = dtree.predict(xTestStd)
+yPred = knn.predict(xTestStd)
 print("Missclassified examples: %d" % (yTest != yPred).sum())
 print("Accuracy: %.3f" % accuracy_score(yTest, yPred))
 
 xCombinedStd = np.vstack((xTrainStd, xTestStd))
 yCombined = np.hstack((yTrain, yTest))
 
-plotDecisionRegions(X=xCombinedStd, y=yCombined, classifier=dtree, testIdx=range(105, 150))
+plotDecisionRegions(X=xCombinedStd, y=yCombined, classifier=knn, testIdx=range(105, 150))
 plt.xlabel("Petal Length (Standardized)")
 plt.ylabel("Petal Width (Standardized)")
 plt.legend(loc="upper left")
@@ -47,5 +51,5 @@ plt.tight_layout()
 plt.show()
 
 
-tree.plot_tree(dtree)
-plt.show()
+#tree.plot_tree(dtree)
+#plt.show()
